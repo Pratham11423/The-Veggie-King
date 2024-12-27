a4f48cc4b4a41b253353ea7a78e06d7c34112e63
Pratham Vishwakarma <prathamdhiman562@gmail.com>
2024-12-27 18:08:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JAI SHREE RAM
+
+document.getElementById('about-us').scrollIntoView({ behavior: 'smooth' });
+
+document.getElementById('menu').scrollIntoView({ behavior: 'smooth' });
+
+document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
